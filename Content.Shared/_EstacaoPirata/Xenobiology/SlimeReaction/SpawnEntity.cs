8a2bf25c37630dffa1e2858c834eccb883524603
Caio Schmidt <caiofonseca6@gmail.com>
2024-04-25 15:28:47 -0300

    Implementa reactions fill e spawn

--- Content.Shared/_EstacaoPirata/Xenobiology/SlimeReaction/SpawnEntity.cs
+++ Content.Shared/_EstacaoPirata/Xenobiology/SlimeReaction/SpawnEntity.cs
@@ -1,17 +1,38 @@
-﻿namespace Content.Shared._EstacaoPirata.Xenobiology.SlimeReaction;
+﻿using Robust.Shared.Map;
+
+namespace Content.Shared._EstacaoPirata.Xenobiology.SlimeReaction;
 
 public sealed partial class SpawnEntity : SlimeReagentEffect
 {
+    // TODO: muar isto para uma lista de strings
     [DataField("toSpawn")]
-    public string ToSpawn;
+    public Dictionary<string, int> ToSpawn;
 
-    [DataField("amount")]
-    public int Amount;
-
-    public override void Effect(SlimeReagentEffectArgs args)
+    public override bool Effect(SlimeReagentEffectArgs args)
     {
         // Com os args, que deve ser algo tipo target e outras coisas, fazer o codigo funcionar por aqui, pra nao precisar definir tudo no
         // system de SlimeReaction. Quero apenas chamar em slimeReaction o SlimeReagentEffect.Effect() pra funcionar automatico plss
-        throw new NotImplementedException();
+
+        if (args.ExtractEntity == null)
+            return false;
+
+        foreach (var prototype in ToSpawn)
+        {
+            for (int i = 0; i < prototype.Value; i++)
+            {
+                var transformSystem = args.EntityManager.System<SharedTransformSystem>();
+                var coordinates = transformSystem.GetMapCoordinates(args.ExtractEntity.Value);
+                var randomizedPosition = coordinates.Position;
+
+                randomizedPosition.X = args.RobustRandom.NextFloat(randomizedPosition.X-0.25f, randomizedPosition.X+0.25f);
+                randomizedPosition.Y = args.RobustRandom.NextFloat(randomizedPosition.Y-0.25f, randomizedPosition.Y+0.25f);
+
+                var randomizedCoordinates = new MapCoordinates(randomizedPosition.X, randomizedPosition.Y, coordinates.MapId);
+
+                args.EntityManager.Spawn(prototype.Key, randomizedCoordinates);
+            }
+        }
+
+        return true;
     }
 }
