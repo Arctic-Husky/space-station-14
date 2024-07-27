a7eb53658872c9346bb123a01dd0c056aefa5860
Caio Schmidt <caiofonseca6@gmail.com>
2024-07-27 16:28:55 -0300

    Initial work on crossbreeding

---
+++ Content.Server/_EstacaoPirata/Xenobiology/SlimeCrossbreeding/SlimeCrossbreedingSystem.cs
@@ -0,0 +1,22 @@
+﻿using Content.Shared._EstacaoPirata.Xenobiology.SlimeCrossbreeding;
+using Content.Shared.Interaction.Events;
+
+namespace Content.Server._EstacaoPirata.Xenobiology.SlimeCrossbreeding;
+
+/// <summary>
+/// This handles...
+/// </summary>
+public sealed class SlimeCrossbreedingSystem : EntitySystem
+{
+    /// <inheritdoc/>
+    public override void Initialize()
+    {
+        SubscribeLocalEvent<SlimeCrossbreedingComponent, GettingInteractedWithAttemptEvent>(OnGettingInteractedAttempt);
+    }
+
+    // Interaction with extracts
+    private void OnGettingInteractedAttempt(Entity<SlimeCrossbreedingComponent> ent, ref GettingInteractedWithAttemptEvent args)
+    {
+        Log.Debug($"Interagido com {args.Target}");
+    }
+}
---
+++ Content.Shared/_EstacaoPirata/Xenobiology/SlimeCrossbreeding/SlimeCrossbreedingComponent.cs
@@ -0,0 +1,33 @@
+﻿using Content.Shared._EstacaoPirata.Xenobiology.SlimeReaction;
+using Robust.Shared.Audio;
+
+namespace Content.Shared._EstacaoPirata.Xenobiology.SlimeCrossbreeding;
+
+/// <summary>
+/// This is used for...
+/// </summary>
+[RegisterComponent]
+public sealed partial class SlimeCrossbreedingComponent : Component
+{
+    [DataField("crossbreeds", true, serverOnly: true)]
+    public List<SlimeExtractReactionEntry>? Crossbreeds;
+}
+
+// [DataDefinition]
+// public sealed partial class SlimeCrossbreedingEntry
+// {
+//     [DataField("type", required: true)]
+//     public string Type;
+//
+//     // Ter uma variavel List<string> que contera os reagentes
+//
+//     [DataField("effects", required: true)]
+//     public List<SlimeReagentEffect> Effects = default!;
+//
+//     [DataField("sound")]
+//     public SoundSpecifier? Sound = new SoundPathSpecifier("/Audio/Effects/Chemistry/bubbles.ogg",
+//         new AudioParams
+//         {
+//             Volume = -10
+//         });
+// }
